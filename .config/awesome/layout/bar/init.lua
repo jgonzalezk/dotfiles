89ccae9e2db9bb56b63e9306a742dabfb5d6ceac
Jorge <jorge.gonzalez.q@usach.cl>
2023-08-13 01:55:49 -0400

    Add: init layout

---
+++ .config/awesome/layout/bar/init.lua
@@ -0,0 +1,8 @@
+-- Requierements
+local awful = require("awful")
+local beautiful = require("beautiful")
+local wibox = require("wibox")
+local gears = require("gears")
+local  xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+
